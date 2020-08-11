4bd84da3b63ba7733b6b49a5073d7fbd502ff792
Ann Newcomer <a.f.newcomer@gmail.com>
2020-08-11 13:01:34 -0600

    added proxy to package.json and Access-Control-Allow-Origin to server.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 
+
+app.use((req,res, next)=> {
+    res.header('Access-Control-Allow-Origin', '*')
+    next()
+})//along with proxy in package.json, allows browser to take cross-origin servers
+
+
 app.listen(9000, () => {
     console.log('Server running on port 9000')
 })
